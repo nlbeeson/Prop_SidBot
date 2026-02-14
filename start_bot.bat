bf4a19cd58d5653d156bd195e5b907e87c90242b
Nate Beeson <emtnate@gmail.com>
2026-02-14 07:55:05 -0500

    chore: updated start bot script.

--- start_bot.bat
+++ start_bot.bat
@@ -1,14 +1,9 @@
 @echo off
-:: 1. Force move to the MT5 directory and launch the terminal
-cd /d "C:\Trading\TradingTerminals\MT5_1"
-start "" "C:\Trading\TradingTerminals\MT5_1\terminal64.exe" /portable
+REM Wait a few seconds after login for Windows to be ready
+timeout /t 10 /nobreak
 
-:: 2. Wait 30 seconds for MT5 to establish a broker connection
-ping 127.0.0.1 -n 31 > nul
+REM Change to your bot folder
+cd "C:\Trading\Bots\Bot_1"
 
-:: 3. Force move to the Bot directory
-cd /d "C:\Trading\Bots\Bot_1"
-
-:: 4. Launch Python using the absolute path to the venv and the script
-:: Using 'python.exe' instead of 'pythonw.exe' will let you see the window on your VPS desktop
-"C:\Trading\Bots\Bot_1\venv\Scripts\python.exe" "C:\Trading\Bots\Bot_1\main.py"
+REM Run Python bot and redirect output to a log file
+C:\Trading\Bots\Bot_1\venv\Scripts\python.exe your_bot.py >> "C:\Trading\Bots\Bot_1\bot.log" 2>&1
